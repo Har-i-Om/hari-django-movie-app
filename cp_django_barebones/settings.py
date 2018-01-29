849c5592a8aa0300bc2acc4841d81cd668bd931d
hariom <h.om.slackucp@gmail.com>
2018-01-29 00:02:49 +0000

    changed django_barebones files in master node

--- cp_django_barebones/settings.py
+++ cp_django_barebones/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&9_c9!x27f&m0_(18z%(bxl1p+^h#rzbx8k#tly4bx6c+i@^+q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['l7hdoxyd.apps.lair.io']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'movies'
 ]
 
 MIDDLEWARE = [
