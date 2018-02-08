77f69f1b38418b319607de8e6f94e36d7d6705df
hariom <h.om.slackucp@gmail.com>
2018-02-08 17:28:40 +0000

    Created backend for adding movies

--- movies/views.py
+++ movies/views.py
@@ -12,7 +12,15 @@ def home_page(request):
     return render(request, 'movies/movies_stuff.html', stuff_for_frontend)
 
 def create(request):
-    print('haha')
+    if request.method == 'POST' :
+        data={
+            'Name': request.POST.get('name'),
+            'Pictures': [{'url': request.POST.get('url')}],
+            'Rating': int(request.POST.get('rating')),
+            'Notes': request.POST.get('notes')
+
+        }
+    AT.insert(data)
     return redirect('/')
 
 AT = Airtable(os.environ.get('AIRTABLE_MOVIESTABLE_BASE_ID'), 'Movies', api_key=os.environ.get('AIRTABLE_API_KEY'))
